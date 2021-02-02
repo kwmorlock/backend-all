dc1569d0a9c09e07f4438368152dff801ff14bb1
kwmorlock <kmorlockcodes@gmail.com>
2021-02-02 06:39:57 -0500

    set: server.js serverget

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-//setup
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ api: "Api is up and running!!!" });
+});
+
+module.exports = server;
