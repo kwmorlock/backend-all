29c018c1942b7996915045f053606405b842a64a
kwmorlock <kmorlockcodes@gmail.com>
2021-02-02 07:44:40 -0500

    set: dbconfig.js knexConfig

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,1 +1,9 @@
-//setup
+const knex = require("knex");
+
+const knexConfig = require("../knexfile.js");
+// const environment = process.env.NODE_ENV || "development";
+
+// module.exports = knex(knexconfig[environment]);
+module.exports = knex(knexConfig.development);
+
+// ('development') this will break postman
