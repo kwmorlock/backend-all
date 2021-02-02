4c64b7d7225a23b76484d8fe71018f7f2ccce766
kwmorlock <kmorlockcodes@gmail.com>
2021-02-02 07:51:26 -0500

    cre: data.db3

---
+++ database/migrations/20210202074632_data.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 50).index().notNullable().unique();
+      tbl.string("password", 20).notNullable();
+      tbl.string("email");
+    })
+    .createTable("stuff", (tbl) => {
+      tbl.increments();
+      tbl.string("title", 50).index().notNullable();
+      tbl.string("description", 2000).notNullable();
+      tbl.string("tags", 50).index().notNullable();
+      tbl
+        .integer("usersId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("stuff")
+    .dropTableIfExists("users")
+};
