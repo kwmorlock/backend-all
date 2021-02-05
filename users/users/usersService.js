c0fe7b1e5743f400387471b5d3d750b679723bd7
kwmorlock <kmorlockcodes@gmail.com>
2021-02-05 02:35:04 -0500

    set: usersService.js isValid

--- users/users/usersService.js
+++ users/users/usersService.js
@@ -1,1 +1,10 @@
-//setup
+module.exports = {
+    isValid,
+  };
+  
+  function isValid(user) {
+    return Boolean(
+      user.username && user.password && typeof user.password === "string"
+    );
+  }
+  
