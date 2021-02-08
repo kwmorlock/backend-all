de145a4ae5a1d2f9b44c97c52efffb2562ec3feb
kwmorlock <kmorlockcodes@gmail.com>
2021-02-08 05:29:18 -0500

    set: informationRouter.js get

--- information/informationrouter/informationRouter.js
+++ information/informationrouter/informationRouter.js
@@ -1,1 +1,15 @@
-//setup
+const router = require("express").Router();
+
+const Information = require("./informationRouter");
+
+router.get("/", (req, res) => {
+  Information.find()
+    .then((information) => {
+      res.status(200).json(information);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error });
+    });
+});
+
+module.exports = router;
