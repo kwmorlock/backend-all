6635e381808258b3acf0a962df2b10be14191fc2
kwmorlock <kmorlockcodes@gmail.com>
2021-02-09 08:17:13 -0500

    set: InformationRouter.js get id

--- information/informationrouter/informationRouter.js
+++ information/informationrouter/informationRouter.js
@@ -12,4 +12,20 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Information.findById(id)
+    .then((information) => {
+      if (information) {
+        res.status(200).json(information);
+      } else {
+        res.status(404).json({ message: "Could not find that information" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error });
+    });
+});
+
 module.exports = router;
