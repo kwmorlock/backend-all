993b0dbe9703e1329144eda17e6802521e9a6c2e
kwmorlock <kmorlockcodes@gmail.com>
2021-02-14 08:53:51 -0500

    set: server.js paths

--- api/server.js
+++ api/server.js
@@ -2,11 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const authenticate = require("../auth/authMiddleware");
+
+const usersAuthRouter = require("../auth/authUsersRouter");
+const informationRouter = require("../information/informationrouter/informationRouter");
+const usersRouter = require("../users/users/usersRouter");
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use("/api/users/users", authenticate, usersRouter);
+server.use("/api/users/auth", usersAuthRouter);
+server.use("/api/info", authenticate, informationRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "Api is up and running!!!" });
